11955a6dbe787c31fa8bcd5e4cf5be5037e56444
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-16 20:27:41 +0700

    options lualine theme molokai

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -140,6 +140,11 @@ local plugins = {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup {
+        options = { theme = "molokai" },
+      }
+    end,
   },
 }
 return plugins
