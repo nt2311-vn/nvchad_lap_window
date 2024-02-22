41665be5a344ed9b95259ac7e7ee28666611614a
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-22 15:06:52 +0700

    Add peek to preview markdown

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -147,7 +147,15 @@ local plugins = {
   --   end,
   -- },
   {
-    "davidgranstrom/nvim-markdown-preview",
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      -- refer to `configuration to change defaults`
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
   },
 }
 return plugins
