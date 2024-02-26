9cfefab5ddb42b5076ccbcb47d855c82c490de7b
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-26 15:51:47 +0700

    Add hidden file to telescope search

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -247,41 +247,9 @@ local default_plugins = {
       dofile(vim.g.base46_cache .. "telescope")
       local telescope = require "telescope"
       telescope.setup {
-        defaults = {
-          vimgrep_arguments = {
-            "rg",
-            "--follow", -- Follow symbolic links
-            "--hidden", -- Search for hidden files
-            "--no-heading", -- Don't group matches by each file
-            "--with-filename", -- Print the file path with the matched lines
-            "--line-number", -- Show line numbers
-            "--column", -- Show column numbers
-            "--smart-case", -- Smart case search
-
-            "--glob=!**/.git/*",
-            "--glob=!**/.idea/*",
-            "--glob=!**/.vscode/*",
-            "--glob=!**/build/*",
-            "--glob=!**/dist/*",
-            "--glob=!**/yarn.lock",
-            "--glob=!**/package-lock.json",
-          },
-        },
         pickers = {
           find_files = {
             hidden = true,
-            find_command = {
-              "rg",
-              "--files",
-              "--hidden",
-              "--glob=!**/.git/*",
-              "--glob=!**/.idea/*",
-              "--glob=!**/.vscode/*",
-              "--glob=!**/build/*",
-              "--glob=!**/dist/*",
-              "--glob=!**/yarn.lock",
-              "--glob=!**/package-lock.json",
-            },
           },
         },
       }
