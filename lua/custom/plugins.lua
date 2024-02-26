219dc4e9c9ed01d33e7a357bb829234c3fa9d010
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-26 09:10:06 +0700

    Add vim autosession

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -147,5 +147,24 @@ local plugins = {
   --   end,
   -- },
   { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+  {
+    "rmagatti/auto-session",
+    event = "VimEnter",
+    config = function()
+      require("auto-session").setup {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/AppData/", "D:/Work/Project" },
+        session_lens = {
+          buftypes_to_ignore = {},
+          load_on_setup = true,
+          theme_conf = { border = true },
+          previewer = false,
+        },
+        vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
+          noremap = true,
+        }),
+      }
+    end,
+  },
 }
 return plugins
