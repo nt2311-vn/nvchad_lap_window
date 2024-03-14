fe367fdbc2089c03a5815c3d324a6b5b51ee9c18
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-14 14:28:39 +0700

    formatter nvim config

--- lua/custom/configs/formatter.lua
+++ lua/custom/configs/formatter.lua
@@ -1,8 +1,4 @@
-local formatter = require "formatter"
-local augroup = vim.api.nvim_create_augroup
-local autocmd = vim.api.nvim_create_autocmd
-
-formatter.setup {
+local M = {
   filetype = {
     html = { require("formatter.filetypes.html").prettier },
     css = { require("formatter.filetypes.css").prettier },
@@ -12,8 +8,8 @@ formatter.setup {
   },
 }
 
-augroup("__formatter__", { clear = true })
-autocmd("BufWritePost", {
-  group = "__formatter__",
-  command = ":FormatWrite",
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  command = "FormatWriteLock",
 })
+
+return M
