a21825bc0a04af23ffe9a7968aa6bc9b068a4403
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-14 15:31:21 +0700

    none-ls prettier to format front end

--- lua/custom/configs/none-ls.lua
+++ lua/custom/configs/none-ls.lua
@@ -12,6 +12,7 @@ null_ls.setup {
     null_ls.builtins.formatting.scalafmt,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.dart_format,
+    null_ls.builtins.formatting.prettier,
 
     -- diagnostics
     null_ls.builtins.diagnostics.yamllint,
