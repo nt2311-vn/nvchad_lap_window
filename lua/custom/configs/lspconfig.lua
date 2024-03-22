307e8b232ca2038f53ab26ad36e8bf1d6d70e197
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-22 15:34:20 +0700

    add volar

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -160,3 +160,33 @@ lspconfig.vuels.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
+
+local function get_typescript_server_path(root_dir)
+  local global_ts = "C:/Users/NguyenToan/node_modules/typescript/lib"
+  local found_ts = ""
+
+  local function check_dir(path)
+    found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+
+    if util.path.exists(found_ts) then
+      return path
+    end
+  end
+
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
+lspconfig.volar.setup {
+  cmd = { "vue-language-server", "--stdio" },
+  filetypes = { "vue" },
+  on_attach = on_attach,
+  capabilities = capabilities,
+
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+  end,
+}
