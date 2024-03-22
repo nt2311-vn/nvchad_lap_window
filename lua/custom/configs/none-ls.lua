437c6625d9b53e7b18f92c8e445074620c214369
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-22 15:56:47 +0700

    recert

--- lua/custom/configs/none-ls.lua
+++ lua/custom/configs/none-ls.lua
@@ -18,9 +18,6 @@ null_ls.setup {
 
     -- diagnostics
     null_ls.builtins.diagnostics.yamllint,
-    null_ls.builtins.diagnostics.eslint_d.with {
-      filetypes = { "javascriptreact", "typescriptreact", "vue", "svelte" },
-    },
   },
   on_attach = function(client, bufnr)
     if client.supports_method "textDocument/formatting" then
